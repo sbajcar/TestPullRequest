6d5c88cf53676c08f719e09c6458fa511a912255
Oceane Andreis <ocan3794@colorado.edu>
2020-10-28 23:24:27 -0600

    Create PrintDictionary.js
    2 errors

---
+++ PrintDictionary.js
@@ -0,0 +1,13 @@
+function Dict()
+{
+  var dict = {};
+  var dict = {
+  "Python": "Super fun",
+  "C++": "It's ok I guess",
+  "C#: "wHo",
+  };
+  for(var key in dict) {
+  var value = dict[key]
+  }
+}
+Dict()
