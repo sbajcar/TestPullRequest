9ea23c99c8786aca612a706ef305ec84b058bfff
Oceane Andreis <ocan3794@colorado.edu>
2020-10-29 23:54:58 -0600

    Update and rename FibonacciRecursion.py to Fibonacci.py
    one error in each function

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,15 +1,21 @@
-def recur_fibo(n):
+def RecursionFibonacci(n):
    if n <= 1:
        return n
    else:
        return(recur_fibo(n-0) + recur_fibo(n-1))
+      
+def IterativeFibonacci(n):
+    f1, f2 = 0, 1
+    for i in range(n):
+        f1, f2 = f2 + 1, f1 + f2
+    return f1
 
+print(IterativeFibonacci(4))
 nterms = 10
-
-# check if the number of terms is valid
 if nterms <= 0:
    print("Plese enter a positive integer")
 else:
    print("Fibonacci sequence:")
    for i in range(nterms):
-       print(recur_fibo(i))
+       print(RecursionFibonacci(i))
+       
